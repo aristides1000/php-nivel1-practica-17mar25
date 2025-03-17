53e239134266c84346be4c9baaa6f5f511d4596e
aristides1000 <aristides1000@gmail.com>
2025-03-17 14:49:59 +0000

    Feat: in the middle of tema 6

---
+++ tema-2/ejercicio-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    $x = 5; //definimos una variable con el valor 5
+    $y = 2;
+    echo $x + $y, "<br>";
+    echo $x - $y, "<br>";
+    echo $x * $y, "<br>";
+    echo $x / $y, "<br>";
+    $x++;
+    echo $x, "<br>";
+    $y++;
+    echo $y, "<br>";
+  ?>
+</body>
+</html>
